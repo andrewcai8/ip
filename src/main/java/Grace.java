4fbb92616b48e3a028556e74b05f1e31efcbd235
Andrew Cai <andrewca78@gmail.com>
2025-08-31 22:06:06 +0800

    list command prints list of tasks

--- src/main/java/Grace.java
+++ src/main/java/Grace.java
@@ -19,6 +19,12 @@ public class Grace {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (input.equals("list")){
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < taskCount; i++){
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("____________________________________________________________");
             } else {
                 tasks[taskCount] = input;
                 taskCount++;
