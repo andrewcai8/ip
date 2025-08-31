a38cbd78ea91857da816dcb0011248cc4759d536
Andrew Cai <andrewca78@gmail.com>
2025-08-31 22:05:36 +0800

    added ability to add tasks to list

--- src/main/java/Grace.java
+++ src/main/java/Grace.java
@@ -4,12 +4,13 @@ import java.util.Scanner;
 public class Grace {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Grace!!");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
-
-
 
         while (true){
             String input = sc.nextLine();
@@ -18,11 +19,14 @@ public class Grace {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
-            }
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
 
-            System.out.println("____________________________________________________________");
-            System.out.println(" " + input);
-            System.out.println("____________________________________________________________");
+                System.out.println("____________________________________________________________");
+                System.out.println(" added: " + input);
+                System.out.println("____________________________________________________________");
+            }
         }
         sc.close();
     }
